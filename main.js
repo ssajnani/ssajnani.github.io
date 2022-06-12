955f5340697c549bf6daba180b1b0317f1f95041
Samar Sajnani <ssajnani@uwo.ca>
2022-06-11 17:53:16 -0700

    update sceneS

--- main.js
+++ main.js
@@ -196,13 +196,13 @@ function createS (scene, positions, radius, zDistance, color=0x000000, opacity=1
 
 function writeS (scene, positions, radius, zDistance, color=0x000000, opacity=1) { 
   var material = new THREE.LineBasicMaterial( { color: 0xffffff, linewidth: 10 } )
-  curve = new THREE.CubicBezierCurve3(
+  curve = new THREE.SplineCurve([
     new THREE.Vector3( positions[0][1], positions[0][0], zDistance),
     new THREE.Vector3( positions[1][1], positions[1][0], zDistance),
     new THREE.Vector3( positions[2][1], positions[2][0], zDistance),
     new THREE.Vector3( positions[3][1], positions[3][0], zDistance),
     new THREE.Vector3( positions[4][1], positions[4][0], zDistance)
-  );
+  ]);
   var geometry = new THREE.Geometry();
   geometry.vertices = curve.getPoints(50);
 
